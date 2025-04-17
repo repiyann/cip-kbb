9e51a0af87f54eea0894a5eff6fe9a520609975c
repiyann <muhrepiyan26@gmail.com>
2025-04-17 14:57:30 +0700

    feat: custom model from spatie

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -12,4 +12,13 @@ class Permission extends SpatiePermission
         'category',
         'guard_name',
     ];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%');
+            });
+        });
+    }
 }
