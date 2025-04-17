8b0aec0711152e8c3cd21be8e618ec4fc265fed9
repiyann <muhrepiyan26@gmail.com>
2025-04-17 14:55:48 +0700

    fix: update permission

--- app/Http/Controllers/RBAC/PermissionController.php
+++ app/Http/Controllers/RBAC/PermissionController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\RBAC;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Inertia\Response;
-use Inertia\Inertia;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Http\RedirectResponse;
 
@@ -28,35 +26,26 @@ class PermissionController extends Controller
     return redirect()->back()->with('success', 'Permission created successfully');
   }
 
-  public function show(Request $request): Response
-  {
-    $permission = Permission::findById($request->id);
-
-    return Inertia::render('dashboard/rbac/rbac-dashboard', [
-      'permission' => $permission,
-    ]);
-  }
-
   public function update(Request $request): RedirectResponse
   {
-    $permission = Permission::findById($request->id);
-
     $request->validate([
-      'name' => 'required|unique:permissions|min:3',
+      'name' => 'required|min:3|unique:permissions,name,' . $request->id . ',id',
       'category' => 'required|min:3',
       'description' => 'required|min:3',
     ]);
 
-    $permission->name = $request->name;
-    $permission->save();
+    Permission::where('id', $request->id)->update([
+      'name' => $request->name,
+      'description' => $request->description,
+      'category' => $request->category,
+    ]);
 
     return redirect()->back()->with('success', 'Permission updated successfully');
   }
 
   public function destroy(Request $request): RedirectResponse
   {
-    $permission = Permission::findById($request->id);
-    $permission->delete();
+    Permission::where('id', $request->id)->delete();
 
     return redirect()->back()->with('success', 'Permission deleted successfully');
   }
